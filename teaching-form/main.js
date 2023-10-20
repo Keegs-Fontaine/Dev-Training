4e171f9924f441bfa16dd249fa635bdeb2948122
keegan <keegan@dc-23-student-073.dhcp.bsu.edu>
2023-10-20 15:20:05 -0400

    Finished teaching form

--- teaching-form/main.js
+++ teaching-form/main.js
@@ -1,25 +1,19 @@
 const form = document.querySelector(".main__form")
 const submitBtn = document.querySelector(".main__form-submit-btn")
 const checkboxArr = document.querySelectorAll(".main__checkbox-grouping-container input")
-const passwordContainers = document.querySelectorAll("main__form-input-container.password")
+const passwordContainers = document.querySelectorAll(".main__form-input-container.password")
 const firstPassword = document.querySelector("#password-first")
 const confirmPassword = document.querySelector("#password-confirm")
-
-// DELETE LATER
-document.querySelectorAll("input").forEach(input => {
-	input.value = "f@gmail"
-
-	if (input.type === "number") input.value = "12"
-})
-document.querySelector("select").value = "FL"
+const yearsTeaching = document.querySelector("#years-teaching")
 
 passwordContainers.forEach(passwordContainer => {
-	passwordBox = passwordContainer.querySelector("input")
-	showHideBtn = passwordContainer.querySelector(".show-hide-btn")
+	const passwordBox = passwordContainer.querySelector("input")
+	const showHideBtn = passwordContainer.querySelector("button")
 
-	showHideBtn.addEventListener("click", () => console.log(passwordBox, showHideBtn))
-
-	console.log(showHideBtn)
+	showHideBtn.addEventListener("click", e => {
+		e.target.innerText = e.target.innerText === "HIDE" ? "SHOW" : "HIDE"
+		passwordBox.type = passwordBox.type === "text" ? "password" : "text"
+	})
 })
 
 function validateCheckboxes() {
@@ -30,20 +24,20 @@ function validateCheckboxes() {
 	return false
 }
 
-function validatePasswords() {
-	return firstPassword.value === confirmPassword.value
-}
-
 form.addEventListener("submit", e => {
 	e.preventDefault()
-	console.log("FORM SUBMIT")
 
 	if (!validateCheckboxes()) checkboxArr[0].setCustomValidity("Please Select a Checkbox")
-	if (!validatePasswords()) firstPassword.setCustomValidity("Passwords Don't Match")
+	if (firstPassword.value === confirmPassword.value) firstPassword.setCustomValidity("Passwords Don't Match")
+
+	const yearsRemote = document.querySelector("#years-remote")
+	if (parseInt(yearsTeaching.value) < parseInt(yearsRemote.value))
+		yearsTeaching.setCustomValidity("Years Teaching Must be Less Or Equal to Years Remote")
 
 	form.reportValidity()
 })
 submitBtn.addEventListener("click", () => {
 	checkboxArr[0].setCustomValidity("")
 	firstPassword.setCustomValidity("")
+	yearsTeaching.setCustomValidity("")
 })
